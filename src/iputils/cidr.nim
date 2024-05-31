590d4d7223b141aee3060aad8964bdce556e423f
rockcavera <rockcavera@gmail.com>
2024-05-31 02:36:15 -0300

    optimizations in `iputils.cidr.rangeToCidr8()`

--- src/iputils/cidr.nim
+++ src/iputils/cidr.nim
@@ -14,44 +14,32 @@ type
     prefix*: range[0'u8 .. 128'u8]
 
 proc rangeToCidr8[T: Cidr4|Cidr6](a, b: uint8, x: int): seq[T] =
+  result = newSeqOfCap[T](14)
+
   var
-    a = int(a)
-    b = int(b)
-  
-  inc(b)
+    a = uint16(a)
+    b = uint16(b) + 1
 
   while a < b:
-    var
-      cidr: T
-      i = 0
-      n = 1
-    
-    while 0 == (n and a):
-      inc(i)
+    let
+      clzb = 15 - countLeadingZeroBits(b - a)
+      ctza = countTrailingZeroBits(a or 256) # avoid undefined behavior when `a` is zero
+      i = min(clzb, ctza)
 
-      n = n shl 1
+    var cidr: T
 
-      if i >= 8:
-        break
-    
-    while i > 0 and (n + a) > b:
-      dec(i)
-
-      n = n shr 1
-    
     cidr.ip[x] = uint8(a)
+    cidr.prefix = 8'u8 - uint8(i)
 
-    cidr.prefix = uint8(8 - i)
-    
     add(result, cidr)
 
-    inc(a, n)
+    a += 1'u16 shl i
 
 template rangeToCidr(T: typedesc[Cidr4|Cidr6]) =
   var
     a = startIp[x]
     b = endIp[x]
-  
+
   if iLimit == x:
     return rangeToCidr8[T](a, b, iLimit)
 
@@ -65,33 +53,33 @@ template rangeToCidr(T: typedesc[Cidr4|Cidr6]) =
 
     for i in 0 .. high(result):
       result[i].ip[x] = a
-      
+
       result[i].prefix += 8'u8
-    
+
     return result
 
   var
     start0 = true
     end255 = true
-  
+
   for i in countup(y, iLimit):
     if 0'u8 != startIp[i]:
       start0 = false
-      
+
       break
-  
+
   for i in countup(y, iLimit):
     if 255'u8 != endIp[i]:
       end255 = false
 
       break
-  
+
   if not(start0):
     var endIp255: typeof(endIp)
 
     for i in countup(y, iLimit):
       endIp255[i] = 255'u8
-    
+
     when sizeof(T) == 5:
       result = ipv4RangeToCidrHelp(startIp, endIp255, y)
     elif sizeof(T) == 17:
@@ -99,31 +87,31 @@ template rangeToCidr(T: typedesc[Cidr4|Cidr6]) =
 
     for i in 0 .. high(result):
       result[i].ip[x] = a
-      
+
       result[i].prefix += 8'u8
-    
+
     inc(a)
-  
+
   if not(end255):
     var startIp0: typeof(startIp)
 
     for i in countup(y, iLimit):
       startIp0[i] = 0'u8
-    
+
     when sizeof(T) == 5:
       var c = ipv4RangeToCidrHelp(startIp0, endIp, y)
     elif sizeof(T) == 17:
       var c = ipv6RangeToCidrHelp(startIp0, endIp, y)
-    
+
     for i in 0 .. high(c):
       c[i].ip[x] = b
 
       c[i].prefix += 8'u8
-    
+
     add(result, c)
 
     dec(b)
-  
+
   if a <= b:
     add(result, rangeToCidr8[T](a, b, x))
 
@@ -157,12 +145,12 @@ template cidrToRange() =
         result.startIp[i] = 0'u8
 
         result.endIp[i] = 255'u8
-      
+
       return result
 
     if 8'u8 <= prefix:
       result.startIp[x] = ip[x]
-      
+
       result.endIp[x] = ip[x]
 
       dec(prefix, 8)
@@ -179,7 +167,7 @@ template cidrToRange() =
     let n = (1'u8 shl (8'u8 - prefix)) -  1'u8
 
     result.startIp[x] = ip[x] and (n xor 255'u8)
-    
+
     result.endIp[x] = result.startIp[x] + n
 
     return result
@@ -193,7 +181,7 @@ proc cidrToIpv4Range*(cidr: Cidr4): tuple[startIp, endIp: Ipv4] =
 proc cidrToIpv6Range*(cidr: Cidr6): tuple[startIp, endIp: Ipv6] =
   ## Returns a tuple with the ``startIp`` and ``endIp`` range of a CIDR block.
   const iLimit = 15
-  
+
   cidrToRange()
 
 template listAllCidrContainIp(h: uint8) =
